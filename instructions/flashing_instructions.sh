77ad0e704bfda54a5ca9139d138ca6d96fb54204
Agata Staniewicz <as6330@bristol.ac.uk>
2018-08-06 11:53:58 +0100

    upfated instructions

--- instructions/flashing_instructions.sh
+++ instructions/flashing_instructions.sh
@@ -1,4 +1,4 @@
-# flashing the SD card for SOLO recorder
+# flashing the SD card for SOLO recorder on Linux
 # insert SD card
 #open Terminal
 
@@ -15,8 +15,8 @@ df -h
 #should be only /dev/sda (that's the comp harddrive)
 
 #flash the card
-#the operating system is on the desktop: /home/agata/Desktop
+#the operating system is on the desktop:
 
 sudo dd bs=40M if=/home/agata/Desktop/sosi-17-11-05.img of=/dev/sdb ; sync ; sync
 
-# change sampling rate to 8KHz and channels to stereo
+# change sampling rate to 8KHz and channels to mono
